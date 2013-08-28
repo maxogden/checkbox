46a0b0b6162cb453cdc9e19d905b08ccd9d3c30c
kristoferjoseph <kristoferjoseph@gmail.com>
2013-08-28 11:22:27 -0700

    Adding gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
             usageguides: {
                 options: {
                     source: 'css',
-                    destination: "demo",
+                    destination: "",
                     template: "node_modules/topdoc-theme/",
                     templateData: {
                       "title": "Topcoat",
